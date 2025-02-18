91b4a75225ad0871ae93b440ea237236f3b77185
Mauricio Jacobo <78543592+mjr-uom@users.noreply.github.com>
2025-02-18 17:07:31 +0000

    Add files via upload

--- gptAgent/__init__.py
+++ gptAgent/__init__.py
@@ -1,1 +1,28 @@
+import random
+import time
+from openai import AzureOpenAI
 
+deployment_name = 'lunar-gpt-4o'
+class OpenAIAgent:
+   def __init__(
+       self,
+       agent_name="OpenAI Agent",
+       system_role = "You are a helpful assistant."
+   ):
+       self.client = AzureOpenAI(
+           api_key="d8d1b0b5a5b94cc7a5fb12a49283fae3",
+           api_version="2024-10-21",
+           azure_endpoint="https://lunarchatgpt.openai.azure.com/"
+       )
+       self.agent_name = agent_name
+       self.system_role = system_role
+       self.deployment_name = 'lunar-gpt-4o'
+   def query(self, message):
+       response = self.client.chat.completions.create(
+           model=deployment_name,
+           messages=[ {"role": "system", "content": self.system_role}, {"role": "user", "content": message}]
+       )
+       for word in response.choices[0].message.content.split():
+           yield word + " "
+           time.sleep(0.05)
+       #return response.choices[0].message.content
